7850c34daa1d93e9f6fd5dedd45eefe27de5d646
ton9q <kuchma_anton@mail.ru>
2018-10-02 01:31:26 +0300

    add drag&drop states

--- src/AppDragDrop.js
+++ src/AppDragDrop.js
@@ -10,6 +10,33 @@ export default class AppDragDrop extends Component {
     ]
   }
   
+  onDragStart = (e, id) => {
+    e.dataTransfer.setData("id", id);
+  }
+
+  onDragOver = (e) => {
+    e.preventDefault();
+  }
+
+  onDrop = (e, cat) => {
+    let id = e.dataTransfer.getData("id");
+
+    let tasks = this.state.tasks.filter((task) => {
+      if(task.name === id) {
+        task.category = cat;        
+      }
+
+      return task;
+    });
+    
+    this.setState({
+      ...this.state,
+      tasks
+    });
+
+    console.log(this.state);
+  }
+
   render() {
     var tasks = {
       wip: [],
@@ -19,6 +46,8 @@ export default class AppDragDrop extends Component {
     this.state.tasks.forEach((t) => {
       tasks[t.category].push(
         <div key={t.name} 
+            draggable  
+            onDragStart = {(e) => this.onDragStart(e, t.name)}
             className="draggable"
             style = {{backgroundColor: t.bgcolor}}
         >
@@ -26,14 +55,22 @@ export default class AppDragDrop extends Component {
         </div>
       )
     });
+
     return(
       <div className="container-drag">
         <h2 className="header">Drag & Drop</h2>
-        <div className="wip">
+        <div className="wip"
+            onDragOver = {(e) => this.onDragOver(e)}
+            onDrop = {(e) => this.onDrop(e, 'wip')}
+        >
           <span className="task-header">WIP</span>
           {tasks.wip}
-        </div>
-        <div className="droppable">
+        </div> 
+
+        <div className="droppable"
+            onDragOver = {(e) => this.onDragOver(e)}
+            onDrop = {(e) => this.onDrop(e, 'complete')}
+        >
           <span className="task-header">COMPLETED</span>
           {tasks.complete}
         </div>
